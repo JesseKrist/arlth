54addfc2d0fad175f2a23e11078e4c53b6063513
Your Name <windowz@live.ru>
2020-09-12 04:25:12 +0300

    xdd

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,27 @@
-document.getElementById("bgm").onclick = function() {open()};
-function open() {
-    document.getElementById("tzt", "bgr_menu").classList.toggle("show");
-}
+
+$(document).ready(function () {
+    $('#lolz').click(function(){
+        $('#tzt').slideToggle(200);
+        $('#lolz').slideUp(200);
+        $('#lolzx').slideDown(200);
+    });
+    $('#lolzx').click(function(){
+        $('#tzt').slideToggle(200);
+        $('#lolzx').slideUp(200);
+        $('#lolz').slideDown(200);
+    });
+
+});
+
+
+
+$(".home").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".promo").offset().top
+    }, 1000);
+});
+$(".own-car").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".terms").offset().top
+    }, 1000);
+});
